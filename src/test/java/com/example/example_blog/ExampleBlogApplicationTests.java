8cc005e945b24f81ed0cac911dac75757bab43f9
Kyosuke Chiba <chiba-ks@bhopari.onmicrosoft.com>
2021-04-15 10:07:46 +0900

    最初のコミット

---
+++ src/test/java/com/example/example_blog/ExampleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.example_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
